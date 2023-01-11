0848a8dada643480995e7a1af21cb5d26d76f261
SAMSONAKANDE <eyitayoaka@gmail.com>
2023-01-11 15:22:33 -0800

    The best argument against democracy is a five-minute conversation with the average voter

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -1,54 +1,19 @@
 #include <stdio.h>
-#include <stdlib.h>
 
 /**
- * main - Main Entry
+ * main - prints all argument it receives
  * @argc: input
  * @argv: input
- * Return: Always 0 (success)
+ * Return: Always 0 (Success)
  */
 int main(int argc, char *argv[])
 {
-	int cents, coins = 0;
+	int i;
 
-	if (argc == 2)
+	for (i = 0; i < argc; i++)
 {
-	cents = atoi(*(argv + 1));
-	while (cents > 0)
-{
-	if (cents % 25 < cents)
-{
-	cents -= 25;
-	coins++;
+
+	printf("%s\n", argv[i]);
 }
-	else if (cents % 10 < cents)
-{
-	cents -= 10;
-	coins++;
-}
-	else if (cents % 5 < cents)
-{
-	cents -= 5;
-	coins++;
-}
-	else if (cents % 2 < cents)
-{
-	cents -= 2;
-	coins++;
-}
-	else if (cents % 1 < cents)
-{
-	cents -= 1;
-	coins++;
-}
-}
-}
-	else
-{
-	printf("Error\n");
-	return (1);
-}
-	printf("%d\n", coins);
 	return (0);
 }
-
